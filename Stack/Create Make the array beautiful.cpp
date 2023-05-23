487f3d7c06f32b520911b665042bb5c25971911c
ajinkyalnangare <89745475+ajinkyalnangare@users.noreply.github.com>
2023-05-23 21:30:45 +0530

    Create Create Make the array beautiful.cpp

---
+++ Stack/Create Make the array beautiful.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> makeBeautiful(vector<int> arr) {
+        // code here
+        
+        stack<int>s;
+        vector<int>ans;
+        
+        for(int i=0;i<arr.size();i++){
+            
+            if(s.empty()){
+                s.push(arr[i]);
+            }
+            else{
+                int top = s.top();
+                if(arr[i]>=0 && top<0 || arr[i]<0 && top>=0){
+                    s.pop();
+                }
+                else{
+                    s.push(arr[i]);
+                }
+            }
+        }
+        if(s.empty()){
+            return ans;
+            
+        }
+        
+        while(!s.empty()){
+            
+            ans.push_back(s.top());
+            s.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+
+        Solution obj;
+        vector<int> res = obj.makeBeautiful(arr);
+        for (int i = 0; i < res.size(); i++) {
+            cout << res[i] << " ";
+        }
+
+        cout << "\n";
+    }
+}
+// } Driver Code Ends
