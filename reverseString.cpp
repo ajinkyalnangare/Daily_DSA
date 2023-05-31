875d87efdb45f09dcf79847d1a4ff28fd74e6135
ajinkyalnangare <89745475+ajinkyalnangare@users.noreply.github.com>
2023-05-31 19:07:59 +0530

    Create reverseString.cpp

---
+++ reverseString.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	char str[40];
+	
+	cout<<"Enter a string"<<endl;
+	cin>>str;
+	
+	cout<<"After the revers\n"<<strrev(str)<<endl;
+	
+	return 0 ;
+}
+
+
+
