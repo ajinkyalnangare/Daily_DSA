1d3b5f01f19f01cc8e6a5ace1de3b6ef33743c3f
Ajinkya Nangare <89745475+ajinkyalnangare@users.noreply.github.com>
2023-06-10 09:56:30 +0530

    Create Aggressive Cows.cpp

---
+++ Aggressive Cows.cpp
@@ -0,0 +1,55 @@
+
+bool isPossible(vector<int> &stalls, int k, int mid){
+
+    int cowCount = 1;
+    int position = stalls[0];
+
+    for(int i=0;i<stalls.size();i++){
+
+        if(stalls[i] - position  >= mid){
+            cowCount++;
+
+            if(cowCount == k){
+                return true;
+            }
+
+            position = stalls[i];
+    }
+        }
+        
+    return false;
+
+}
+
+int aggressiveCows(vector<int> &stalls, int k)
+{
+    //    Write your code here.
+     sort(stalls.begin(),stalls.end());
+     
+    int s = 0;
+    int maxi = -1;
+
+    for(int i=0;i<stalls.size();i++){
+
+        maxi = max(maxi,stalls[i]);
+    }
+
+    int e = maxi;
+    int ans = -1;
+    int mid = s + (e - s)/2;
+
+    while(s <= e){
+
+        if(isPossible(stalls,k,mid)){
+            ans = mid;
+            s = mid + 1;
+        }
+        else{
+            e = mid - 1;
+        }
+
+        mid = s + (e - s)/2;
+    }
+
+    return ans;
+}
