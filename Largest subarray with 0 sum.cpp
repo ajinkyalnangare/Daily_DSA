88343c30b4cbf1e1c5712225ac49fdfc69b0dc75
Ajinkya Nangare <89745475+ajinkyalnangare@users.noreply.github.com>
2023-06-23 17:34:30 +0530

    Create Largest subarray with 0 sum.cpp

---
+++ Largest subarray with 0 sum.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/*You are required to complete this function*/
+
+class Solution{
+    public:
+    int maxLen(vector<int>&a, int n)
+    {   
+        // Your code here
+        
+      unordered_map <int,int> m;
+        int ans = 0,sum = 0;
+        m[0] = -1;
+        for(int i = 0;i < n;i++)
+        {
+          sum += a[i];
+          if(m.count(sum)) 
+          ans = max(ans,i-m[sum]);
+          else m[sum] = i;
+        }
+       return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int m;
+        cin>>m;
+        vector<int> array1(m);
+        for (int i = 0; i < m; ++i){
+            cin>>array1[i];
+        }
+        Solution ob;
+        cout<<ob.maxLen(array1,m)<<endl;
+    }
+    return 0; 
+}
+
+
+
+// } Driver Code Ends
